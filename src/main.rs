36fc2f7d7a324241d2a84b7fe9a639358c8b6fa4
John Gentile <johncgentile17@gmail.com>
2026-02-14 23:42:18 -0500

    basics

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use log::info;
 use pachamama::brokerage::alpaca::alpaca_client::{AccountType, AlpacaClient};
+use std::env::set_var;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    unsafe { set_var("RUST_LOG", "INFO") };
     env_logger::init();
 
     info!("Initiating Pachamama...");
